4fb5e133f3360d1e477259f8d2c97d81baa1aeb6
Sunso <sunso514@naver.com>
2023-02-09 23:34:38 +0900

    [Bronze IV] Title: 기찍 N, Time: 684 ms, Memory: 35528 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+		for (int i = 0; i < a; i++) {
+			System.out.println(a-i);
+		}	
+		
+		
+	}
+	
+}
