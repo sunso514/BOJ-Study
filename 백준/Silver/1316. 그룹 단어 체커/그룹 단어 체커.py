8ee276b7809f9c437b6ea3dfd502c45abc280797
Sunso <sunso514@naver.com>
2023-02-09 23:26:56 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,18 @@
+#goal_e, goal_s, goal_m = map(int, input().split())
+
+
+
+
+N = int(input())
+count = N
+
+for _ in range(N):
+    get = input()
+    for i in range(0, len(get)-1):
+        if get[i] == get[i+1]:
+            pass
+        elif get[i] in get[i+1:]: 
+            count -= 1
+            break
+print(count)
+
