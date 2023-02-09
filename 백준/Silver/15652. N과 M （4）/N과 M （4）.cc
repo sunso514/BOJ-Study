8cc03600f9a93efc0324cbec4c86b3d7b22a22a9
Sunso <sunso514@naver.com>
2023-02-09 23:42:47 +0900

    [Silver III] Title: N과 M (4), Time: 8 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+using namespace std;
+
+//#define MAX 100001 // 최대 정점의 개수
+//#define INF 99999999
+
+//vector<int> list;
+
+int N, M;
+vector<bool> check;
+
+struct Location {
+    int cord, depth;
+    queue<int> moves;
+};
+
+void dfs(Location loc) {
+    if (loc.depth == M) { // 수열 최대 길이에 도달
+        while (!loc.moves.empty()) {
+            cout << loc.moves.front() << " ";
+            loc.moves.pop();
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = loc.cord; i <= N; i++) {
+        if (!check[i]) {
+            queue<int> moves = loc.moves;
+            moves.push(i);
+
+            //check[i] = true; // 방문 처리
+            dfs({ i, loc.depth + 1, moves });
+            check[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin >> N >> M;
+    check.resize(N + 1);
+
+    dfs({ 1, 0 });
+}
