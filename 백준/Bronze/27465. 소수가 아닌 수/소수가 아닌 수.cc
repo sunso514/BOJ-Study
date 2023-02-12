db615f2eac969e368c6f9b46e2d66b4af023a513
Sunso <sunso514@naver.com>
2023-02-12 14:08:11 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <numeric>
+#include <bitset>
+#define test "Test "
+
+using namespace std;
+
+//#define MAX 100001 // 최대 정점의 개수
+//#define INF 99999999
+
+//vector<int> list;
+
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << 999999999;
+}
