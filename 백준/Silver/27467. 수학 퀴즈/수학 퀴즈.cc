80fa46310705ae39ee169d13fbe6cbbbb5594314
Sunso <sunso514@naver.com>
2023-02-12 14:09:17 +0900

    [Silver I] Title: 수학 퀴즈, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/27467. 수학 퀴즈/수학 퀴즈.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <numeric>
+#include <bitset>
+#define test "Test "
+
+using namespace std;
+
+//#define MAX 100001 // 최대 정점의 개수
+//#define INF 99999999
+
+//vector<int> list;
+
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a = 0;
+        cin >> a;
+    }
+    cout << 0 << " " << N;
+
+}
