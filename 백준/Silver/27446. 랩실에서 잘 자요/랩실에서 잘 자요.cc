d6e7d31e1385d7b69025aa299b819bde0c797d6e
Sunso <sunso514@naver.com>
2023-02-15 09:57:13 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.cc
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.cc
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
+
 
 using namespace std;
 
@@ -27,45 +22,33 @@ int main() {
         page[a] = true;
     }
 
-    // 1 2 3 5 6 7 8 9  1 2 3 
-
     bool streak = false;
-    int streaknum = 0;
     int n_streaknum = 0;
     int answer = 0;
     for (int i = 1; i <= N; i++) {
         if (streak == false) {
             if (page[i] == true) continue;
             else {
-                //cout << i << "에서 streak 시작" << endl;
                 streak = true;
                 answer += 7;
             }
         }
         else {
-            if (page[i] == true) { // 이미 찾은 페이지일 때
+            if (page[i] == true) { 
                 n_streaknum++;
                 if (n_streaknum > 2) {
                     answer -= n_streaknum*2-2;
-                    //cout << "빼기:"<<n_streaknum*2-2 << endl;
                     streak = false;
-                    streaknum = 0;
                     n_streaknum = 0;
                 }
-                else {
-                    answer += 2;
-                    //cout <<  i << "에서 더하기:2 그리고 streak은 " << n_streaknum << endl;
-                }
+                else answer += 2;
             }
             else {
                 answer += 2;
                 n_streaknum = 0;
-                //cout << "i :" << i << "," << answer << " " << endl;
             }
-            
         }
     }
     if (streak == true) answer -= n_streaknum*2;
-    
     cout << answer;
 }
