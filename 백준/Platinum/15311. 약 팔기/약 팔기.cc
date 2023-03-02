68032c845381d65d97cccce31ec3e58dd28be908
Sunso <sunso514@naver.com>
2023-03-02 22:38:39 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+int N, M;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int a; cin >> a;
+
+    cout << 2000 << endl;
+    for (int i = 0; i < 999; i++) cout << 1 << " ";
+    for (int i = 0; i < 1001; i++) cout << 1000 << " ";
+
+}
