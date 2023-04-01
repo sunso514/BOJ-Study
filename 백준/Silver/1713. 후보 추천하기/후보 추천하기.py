ebb9fa0ce6bea3b092fb8129320a1360899bd1cf
DongHyun <sunso514@naver.com>
2023-04-01 19:09:13 +0900

    [Silver I] Title: 후보 추천하기, Time: 212 ms, Memory: 39788 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,47 @@
+from collections import deque
+from dataclasses import dataclass
+
+
+@dataclass
+class Vote:
+    num: int = 0
+    count: int = 1
+
+
+def check_vt():  # 3번 규칙
+    mins = 101
+    ind = 1
+    for i in range(len(deq)):
+        if mins > deq[i].count:
+            ind = i
+            mins = deq[i].count
+    deq.remove(deq[ind])
+
+
+def check_jb(a):
+    for i in range(len(deq)):
+        if a == deq[i].num:
+            deq[i].count += 1
+            return True
+    return False
+
+
+deq = deque()
+N, C = int(input()), int(input())
+
+draw = [-1 for i in range(C)]
+lists = list(map(int, input().split()))
+for i in range(C):
+    if not check_jb(lists[i]):
+        ls = Vote()
+        ls.num = lists[i]
+        if len(deq) >= N:
+            check_vt()
+        deq.append(ls)
+ans = []
+for i in range(len(deq)):
+    ans.append(deq[i].num)
+ans.sort()
+
+for i in range(len(ans)):
+    print(ans[i], end=' ')
