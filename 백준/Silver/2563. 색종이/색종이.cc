7493bf32ec3e23ba5b78ebc3d9562cb7ca67c178
DongHyun <sunso514@naver.com>
2023-04-24 18:59:00 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#define test "test "
+#define endl "\n"
+using namespace std;
+
+#define 반복(i, N) for(int (i) = 0; (i) < (N); (i)++)
+#define 만약 if
+#define 입력 cin
+#define 출력 cout
+
+#define 벡터 vector
+#define 정수 int
+#define 공허 void
+
+#define 메인함수 main
+
+
+
+벡터<벡터<정수>> 도화지(100, 벡터<정수>(100));
+정수 넓이;
+
+공허 도화지칠하기(정수 엑, 정수 와) {
+	반복(ㅁ, 10) 반복(ㄴ, 10){
+		만약(도화지[ㅁ+엑][ㄴ+와] == 0) {
+			도화지[ㅁ+엑][ㄴ+와] = 1;
+			넓이++;
+		}
+	}	
+}
+
+
+정수 메인함수(){
+
+	정수 색종이수;
+	입력 >> 색종이수;
+
+	반복(ㅁ, 색종이수) {
+		정수 엑스, 와이;
+		입력 >> 엑스 >> 와이;
+		도화지칠하기(엑스, 와이);
+	}
+	출력 << 넓이;
+
+}
