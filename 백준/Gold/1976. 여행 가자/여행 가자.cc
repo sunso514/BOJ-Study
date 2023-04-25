0bdd04ae7c3afd7109f530b55be40950caf5c259
DongHyun <sunso514@naver.com>
2023-04-25 21:46:59 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#define test "test "
+#define endl "\n"
+using namespace std;
+
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+vector<int> parentN(201, -1);
+int N, M;
+
+#define X loc.x + dx[i]
+#define Y loc.y + dy[i]
+
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+int find(int cord) {
+    if (parentN[cord] < 0) return cord; // 부모가 없음
+    return parentN[cord] = find(parentN[cord]); 
+}
+
+void Union(int x, int y) {
+    int x_parent = find(x);
+    int y_parent = find(y);
+
+    if (x_parent == y_parent) return; // 같은 집합
+    if (parentN[x_parent] > parentN[y_parent]) {
+        parentN[x_parent] += parentN[y_parent];
+        parentN[y_parent] = x_parent;
+    }
+    else {
+        parentN[y_parent] += parentN[x_parent];
+        parentN[x_parent] = y_parent;
+    }
+}
+
+
+int main() {
+
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            int con; cin >> con;
+            if (con == true) {
+                if (find(i) == find(j)) continue;
+                Union(i, j); 
+            }
+        }
+    }
+
+    int start_point;
+    cin >> start_point;
+
+    for (int i = 1; i < M; i++) {
+        int a; cin >> a;
+        if (find(start_point) == find(a)) {
+            start_point = a;
+        }
+        else {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+
+}
