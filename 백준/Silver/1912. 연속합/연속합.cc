1579246bbba365ef5eb7d87a2edf6e5397bee1c6
DongHyun <sunso514@naver.com>
2023-04-26 17:43:36 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,19 +1,9 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#define test "test "
-#define endl "\n"
 using namespace std;
 
 #define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
-
 
 int main() {
 
