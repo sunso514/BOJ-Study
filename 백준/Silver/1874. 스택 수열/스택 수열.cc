9c15b78741feb3962d59eb9bd114da3364e6c65a
DongHyun <sunso514@naver.com>
2023-04-29 14:42:20 +0900

    [Silver II] Title: 스택 수열, Time: 28 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,19 +1,9 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
 #include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#define test "test "
 #define endl "\n"
 using namespace std;
-
-#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
-
 
 int main() {
 
@@ -22,19 +12,17 @@ int main() {
     int N; cin >> N;
 
     int now = 0;
-
     stack<int> tmp;
     stack<int> stk;
     vector<string> ans;
     for (int i = 0; i < N; i++) {
         int a; cin >> a;
-        if (now < a) {
-            while (now < a) {
-                now++;
-                tmp.push(now);
-                ans.push_back("+");
-            }
+        if (now < a) while (now < a) {
+            now++;
+            tmp.push(now);
+            ans.push_back("+");
         }
+
         int t = tmp.top();
         if (t != a){
             cout << "NO";
@@ -45,5 +33,4 @@ int main() {
         tmp.pop();
     }
     for (int i = 0; i < ans.size(); i++) cout << ans[i] << endl;
-
 }
