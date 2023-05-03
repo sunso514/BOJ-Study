a4ff7ce8f8dfadf42938ac37f378ed5445af1f2c
DongHyun <sunso514@naver.com>
2023-05-03 21:47:37 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -6,9 +6,6 @@ lists = [i+1 for i in range(N)]
 
 for i in range(M):
     a, b = map(int, input().split())
-    tmp = lists[b-1]
-    lists[b-1] = lists[a-1]
-    lists[a-1] = tmp
-
+    lists[b-1], lists[a-1] = lists[a-1], lists[b-1]
 for i in range(N):
     print(lists[i], end= " ")
