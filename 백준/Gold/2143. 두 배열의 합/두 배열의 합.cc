15a0ece1fbb69fe9afd6077d189f61b03c938545
DongHyun <sunso514@naver.com>
2023-05-07 14:49:40 +0900

    [Gold III] Title: 두 배열의 합, Time: 100 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <ranges>
+#define test "test "
+#define endl "\n"
+using namespace std;
+
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+struct Location {
+    int x, y, color;
+};
+
+int T, N, M;
+
+
+vector<long long> A, B;
+
+
+void setting() { // A, B 배열을 구하고 누적합을 받아놓기
+    cin >> N; 
+    for (int i = 0; i < N; i++) {
+        long long wow; cin >> wow;
+        A.push_back(wow);
+    }
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        long long wow; cin >> wow;
+        B.push_back(wow);
+    }
+    for (int i = 0; i < N; i++) {
+        long long sum = A[i];
+        for (int j = i + 1; j < N; j++) {
+            sum += A[j];
+            A.push_back(sum);
+        }
+    }
+    for (int i = 0; i < M; i++) {
+        long long sum = B[i];
+        for (int j = i + 1; j < M; j++) {
+            sum += B[j];
+            B.push_back(sum);
+        }
+    }
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+}
+
+long long LowerBound(int x) {
+    int low = -1, high = B.size();
+    while (low + 1 < high) {
+        int mid = (low + high) / 2;
+        if (B[mid] < x) low = mid;
+        else high = mid;
+    }
+    return high;
+}
+
+long long UpperBound(int x) {
+    int low = -1, high = B.size();
+    while (low + 1 < high) {
+        int mid = (low + high) / 2;
+        if (B[mid] <= x) low = mid;
+        else high = mid;
+    }
+    return high;
+}
+
+int main() {
+
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+ 
+    cin >> T; // 몇이 되어야 하는가
+
+    setting(); // 배열 관련 입력
+
+    long long ans = 0;
+    for (int i = 0; i < A.size(); i++) {
+        long long low = LowerBound(T - A[i]);
+        long long high = UpperBound(T - A[i]);
+        ans += high - low;
+    }
+    cout << ans;
+
+}
