2e1be8efb5da06d5d38148c7a33263933fed7ab1
DongHyun <sunso514@naver.com>
2023-05-08 17:49:11 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,8 +1,8 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-stackoverflow = []
+st = []
 for i in range(N):
     tmp = int(input())
-    stackoverflow.append(tmp) if tmp != 0 else stackoverflow.pop() if len(stackoverflow) > 0 else print(end = "")
-print(sum(stackoverflow))
+    st.append(tmp) if tmp != 0 else st.pop()
+print(sum(st))
