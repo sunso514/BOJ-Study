22ca4af5356cc2f272314e829da08d28cd9b1b03
DongHyun <sunso514@naver.com>
2023-05-29 06:40:01 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.py
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -1,13 +1,11 @@
 N, M = map(int, input().split())
 
-sixmin = 1001
-onemin = 1001
+sixmin, onemin = 1001, 1001
+
 for i in range(M):
     a, b = map(int, input().split())
-    if a < sixmin:
-        sixmin = a
-    if b < onemin:
-        onemin = b
+    sixmin = min(a, sixmin)
+    onemin = min(b, onemin)
 
 goal = N
 ans = 0
@@ -15,15 +13,11 @@ if sixmin >= onemin*6:
     print(onemin*N)
     exit()
 
-
 while goal > 0:
     if goal > 6:
         ans += sixmin
     else:
-        if goal*onemin <= sixmin:
-            ans += goal*onemin
-        else:
-            ans += sixmin
+        ans += (goal*onemin if goal*onemin <= sixmin else sixmin)
     goal -= 6
 
 print(ans)
