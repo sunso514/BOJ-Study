ab254e38db85357177ca7fd0d7a3a62d4efaad4e
DongHyun <sunso514@naver.com>
2023-05-31 07:08:39 +0900

    [Silver V] Title: Data Packing (Small), Time: 84 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver/12234. Data Packing （Small）/Data Packing （Small）.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+T =int(input())
+
+for T in range(T):
+    N, X = map(int, input().split())
+    lst = list(map(int, input().split()))
+
+    lst.sort()
+    que = deque(lst)
+    
+    disk = 0
+    while len(que) > 0:
+        if que[0] + que[len(que)-1] <= X:
+            if len(que) > 0:
+                que.popleft()
+        if len(que) > 0:
+            que.pop()
+        disk += 1
+    print(f"Case #{T+1}: {disk}")
+
