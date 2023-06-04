5b19b8022cc991745aa006d8f980dca4ffeaf709
DongHyun <sunso514@naver.com>
2023-06-04 09:49:31 +0900

    [Gold IV] Title: DSLR, Time: 2460 ms, Memory: 2448 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,24 +1,15 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
 #include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
 #include <deque>
-#include <ranges>
-#include <set>
-#define test "test "
+
 #define endl "\n"
 using namespace std;
 
-#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
-
 int now, goal;
 struct JongGang {
-    int index, depth, parent;
+    int index, parent;
     char how;
 };
 
@@ -36,7 +27,6 @@ void bfs() {
         if (haja.index == goal) {
             graph[haja.index].parent = haja.parent;
             graph[haja.index].how = haja.how;
-            graph[haja.index].depth = haja.depth;
             return;
         }
 
@@ -45,12 +35,11 @@ void bfs() {
         visit[haja.index] = true;
         graph[haja.index].parent = haja.parent;
         graph[haja.index].how = haja.how;
-        graph[haja.index].depth = haja.depth;
         
-        que.push({ (haja.index * 2) % 10000 , haja.depth + 1, haja.index , 'D'});
-        que.push({ haja.index == 0 ? 9999 : haja.index-1 , haja.depth + 1, haja.index , 'S'});
-        que.push({(haja.index % 1000) * 10 + (haja.index / 1000), haja.depth + 1, haja.index ,'L'});
-        que.push({haja.index / 10 + (haja.index % 10) * 1000, haja.depth + 1, haja.index ,'R' });
+        que.push({ (haja.index * 2) % 10000 , haja.index , 'D'});
+        que.push({ haja.index == 0 ? 9999 : haja.index-1 , haja.index , 'S'});
+        que.push({(haja.index % 1000) * 10 + (haja.index / 1000), haja.index ,'L'});
+        que.push({haja.index / 10 + (haja.index % 10) * 1000, haja.index ,'R' });
         
     }
 }
@@ -63,14 +52,14 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     visit.resize(10001);
-    graph.resize(10001, { -1, -1, -1, 'N'});
+    graph.resize(10001, { -1, -1, 'N'});
 
     int N; cin >> N;
     for (int i = 0; i < N; i++) {
         fill(visit.begin(), visit.end(), 0);
         cin >> now >> goal;
 
-        que.push({ now, 0, -5 , 'N'});
+        que.push({ now, -5 , 'N'});
         bfs();
 
         deque<char> jongganghaja;
@@ -87,7 +76,6 @@ int main() {
         while (!que.empty()) que.pop();
         for (int j = 1; j <= 10001; j++) {
             graph[i].parent = -1;
-            graph[i].depth = -1;
             graph[i].how = 'N';
         }
     }
