d1e9e0dfc112a2955881ef292a93b917ab4618c5
DongHyun <sunso514@naver.com>
2023-06-19 10:04:55 +0900

    [Silver I] Title: Кондиционеры, Time: 1636 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/21512. Кондиционеры/Кондиционеры.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#define test "test "
+#define endl "\n"
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+int N, M;
+
+
+struct AirInfo {
+    int watt, price;
+};
+
+bool compare(AirInfo & p1, AirInfo & p2) {
+    if (p1.price == p2.price) {
+        return p1.watt < p2.watt;
+    }
+    else return p1.price < p2.price;
+
+}
+vector<int> needWatt;
+deque<AirInfo> aircon;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int inp; cin >> inp;
+        needWatt.push_back(inp);
+    }
+
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int a, b; cin >> a >> b;
+        aircon.push_back({ a, b });
+    }
+    // 최적의 경우의 수만 남겨놓는다.
+
+    sort(aircon.begin(), aircon.end(), compare);
+
+    int res = 0;
+
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < aircon.size(); j++) {
+            if (needWatt[i] <= aircon[j].watt) {
+                res += aircon[j].price;
+                break;
+            }
+        }
+    }
+    cout << res;
+}
