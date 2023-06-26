be2f1344afda908266018a2b5be98da765b5bb26
DongHyun <sunso514@naver.com>
2023-06-26 11:46:01 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
@@ -0,0 +1,8 @@
+print("Gnomes:")
+for _ in range(int(input())) :
+    lst = list(map(int, input().split()))
+    res = True
+    if (lst[1] - lst[0] >= 0 and lst[2] - lst[1] >= 0) or (lst[2] - lst[1] < 0 and lst[1] - lst[0] < 0): 
+        print("Ordered")
+    else: 
+        print("Unordered")
