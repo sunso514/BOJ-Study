820e2c53230b2c724a8943b3e0973a8bd81f2e76
DongHyun <sunso514@naver.com>
2023-07-07 21:59:30 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+int N, K, M;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	deque<int> deq;
+
+	cin >> N;
+	FOR(i, N) deq.push_back(i + 1);
+	
+	while (true) {
+		if (deq.size() == 1) break;
+		deq.pop_front();
+
+		if (deq.size() == 1) break;
+		deq.push_back(deq.front());
+		deq.pop_front();
+	}
+	cout << deq[0];
+}
