2e5bfb3c4f908b78f9165cd74abb17e51a80b40d
DongHyun <sunso514@naver.com>
2023-07-22 14:49:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,28 +1,15 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
-#define endl "\n"
 
 using namespace std;
-#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
 
-int N, M, T;
+int N, S;
 int res = 0;
-vector<int> lst(20, 0);
+int lst[20];
 
 void backtracking(int now, int sum) {
 	if (now == N) return;
-	if (sum + lst[now] == M) res++;
+	if (sum + lst[now] == S) res++;
 
 	backtracking(now + 1, sum + lst[now]);
 	backtracking(now + 1, sum);
@@ -33,11 +20,9 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> lst[i];
-	}
+	cin >> N >> S;
+	for (int i = 0; i < N; i++) cin >> lst[i];
+
 	backtracking(0, 0);
 	cout << res;
-
 } 
