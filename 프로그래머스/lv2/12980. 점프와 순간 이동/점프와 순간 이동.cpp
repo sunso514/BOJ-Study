f5f1d32e925014901bce8f2183d99cbca529f16e
DongHyun <sunso514@naver.com>
2023-07-23 00:23:02 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -4,12 +4,8 @@ using namespace std;
 int solution(int n)
 {
     int ans = 0;
-    
     while(n > 0){
-        if (n % 2 == 0){
-            n /= 2;
-            continue;
-        }
+        if (n % 2 == 0) n /= 2;
         else{
             n--;
             ans++;
