12bc06b75970aa90a8737a8d710a1dbd3eb8e9b9
DongHyun <sunso514@naver.com>
2023-07-23 12:33:32 +0900

    [Silver II] Title: 나무꾼 이다솜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.cc
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.cc
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
-#define endl "\n"
 
 using namespace std;
 #define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
@@ -48,6 +37,7 @@ int main() {
 			// 나누어 떨어지지 않았다면
 			if (t_size % L != 0) cost = K * C;
 			else cost = (K - 1) * C;
+            
 			if (K * W * L - cost > 0) tmp += K * W * L - cost;
 			//cout << tmp << " ";
 		}
