6f3278d74aef8f14d3fc7ba2b619b93437f88811
DongHyun <sunso514@naver.com>
2023-07-23 13:55:03 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -2,22 +2,10 @@
 #include <algorithm>
 #include <cstring>
 #include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
 #include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
-#define endl "\n"
 
 using namespace std;
 #define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
-
-int N, C;
-long long W;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -34,11 +22,16 @@ int main() {
 
 	char hol = '0';
 	for (int i = 25; i >= 0; i--) {
+        
+        // 한 번도 나온적이 없다면 패스
 		if (alp[i] == 0) continue;
+        
+        // 짝수라면?
 		if (alp[i] % 2 == 0) {
 			FOR(j, alp[i] / 2) ans.push_front(i + 'A');
 		}
 		else {
+            // 홀수개인 문자가 2개 이상이면 탈락!
 			if (hol != '0') {
 				cout << "I'm Sorry Hansoo";
 				exit(0);
@@ -49,11 +42,11 @@ int main() {
 			}
 		}
 	}
+    
+    // 마무리
 	int siz = ans.size();
 	if (hol != '0') ans.push_back(hol);
-	for(int i = siz-1; i >= 0; i--) {
-		ans.push_back(ans[i]);
-	}
+	for(int i = siz-1; i >= 0; i--) ans.push_back(ans[i]);
 
 
 	FOR(i, ans.size()) cout << ans[i];
