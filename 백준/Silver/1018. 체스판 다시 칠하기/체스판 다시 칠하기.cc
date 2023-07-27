ddde7e47b9ad748f693c12f12acb2006b067f8ca
DongHyun <sunso514@naver.com>
2023-07-27 14:07:13 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
-#define endl "\n"
 
 using namespace std;
 #define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
@@ -36,23 +25,19 @@ int main() {
 
 	FOR(bti, N - 7) FOR(btj, M - 7) {
 		int check = 0;
-		//cout << endl;
+        
+        // WBWB, BWBW
 		FOR(tmc, 2) {
 			int cnt = 0;
 			FOR(i, 8) {
 				check = (tmc + i) % 2;
 				FOR(j, 8) {
-					//cout << ((check == 0) ? 'W' : 'B');
 					if (vec[i+bti][j+btj] != check) cnt++;
 					check = (check + 1) % 2;
 				}
-				//cout << endl;
 			}
-			//cout << cnt;
-			//cout << endl;
 			res = min(res, cnt);
 		}
 	}
-
 	cout << res;
 } 
