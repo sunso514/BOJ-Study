c009786ae86f6d10c7e1540f6f4c6af7c1edf05e
DongHyun <sunso514@naver.com>
2023-07-28 12:52:17 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,23 +1,9 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
 #define endl "\n"
 
 using namespace std;
-#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
-
-int N, M;
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
