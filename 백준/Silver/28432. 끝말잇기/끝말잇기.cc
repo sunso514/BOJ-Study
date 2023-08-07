715e260cfb1fe76eddcd55d78578c2e39d61ad43
DongHyun <sunso514@naver.com>
2023-08-07 12:52:12 +0900

    [Silver V] Title: 끝말잇기, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/28432. 끝말잇기/끝말잇기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#define test "test "
+#define endl "\n"
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N; cin >> N;
+
+    map<string, int> mp;
+    map<int, string> mpt;
+
+    int loc = -1;
+
+    FOR(i, N) {
+        string word; cin >> word;
+
+        if (word == "?") loc = i;
+        else {
+            mp[word] = i;
+            mpt[i] = word;
+        }
+    }
+    int hb; cin >> hb;
+    FOR(i, hb) {
+        string a; cin >> a;
+        if (mp.find(a) != mp.end()) continue;
+
+        if (loc > 0) {
+            //cout << mpt[loc - 1] << " ";
+            if (a[0] != mpt[loc - 1][mpt[loc - 1].length() - 1]) continue;
+        }
+        if (loc < N - 1) {
+            //cout << mpt[loc + 1] << " ";
+            if (a[a.length() - 1] != mpt[loc + 1][0]) continue;
+        }
+        cout << a;
+    }
+}
