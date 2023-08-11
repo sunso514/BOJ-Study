42d0b934e7da7005bb4f81362404d66adcb46319
DongHyun <sunso514@naver.com>
2023-08-11 15:25:34 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#define test "test "
+#define endl "\n"
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+vector<vector<int>> rgb;
+vector<vector<int>> dp;
+
+int N;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    rgb.resize(N, vector<int>(3));
+    dp.resize(N, vector<int>(3, 1000000007));
+
+
+    FOR(i, N) FOR(j, 3) cin >> rgb[i][j];
+
+    FOR(i, 3)  dp[0][i] = rgb[0][i];
+
+    for (int i = 1; i < N; i++) {
+        FOR(j, 3) {
+            dp[i][j] = min(dp[i - 1][(j + 1) % 3] + rgb[i][j], dp[i][j]);
+            dp[i][j] = min(dp[i - 1][(j + 2) % 3] + rgb[i][j], dp[i][j]);
+        }
+    }
+    cout << min(dp[N - 1][0], min(dp[N - 1][1], dp[N - 1][2]));
+}
