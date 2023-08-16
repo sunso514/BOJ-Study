cb2b3b3542d06cb11fb2e4c8396cdd954b8d525c
DongHyun <sunso514@naver.com>
2023-08-16 22:07:41 +0900

    [Silver I] Title: 흙길 보수하기, Time: 180 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.cc
+++ 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.cc
@@ -1,34 +1,19 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-#define test "test "
-#define endl "\n"
 
 using namespace std;
 #define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
 
 int N, L;
 
-
-
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
 
     cin >> N >> L;
-
-    int count = 0;
     vector<pair<int, int>> gp;
-
+    
     FOR(i, N) {
         int a, b;
         cin >> a >> b;
@@ -37,6 +22,7 @@ int main() {
 
     sort(gp.begin(), gp.end());
 
+    int count = 0;
     int x = 0;
 
     FOR(i, N) {
