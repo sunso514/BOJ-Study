be73c3ba8c6097b3a4ce6582069cd61584ce0226
DongHyun <sunso514@naver.com>
2023-08-21 16:40:28 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#define test "test "
+#define endl "\n"
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+int N, M;
+
+vector<vector<int>> bigger;
+vector<vector<int>> smaller;
+vector<int> visit;
+
+int countv = 0;
+
+void dfs(int loc, bool big) {
+    for (auto x : (big == true) ? bigger[loc] : smaller[loc]){
+        if (!visit[x]) {
+            visit[x] = true;
+            countv++;
+            dfs(x, big);
+        }
+    }
+    
+}
+
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N >> M;
+
+    int mid = (N + 1) / 2;
+
+    bigger.resize(N+1, vector<int>());
+    smaller.resize(N+1, vector<int>());
+    visit.resize(N + 1);
+
+    FOR(i, M) {
+        int a, b; cin >> a >> b;
+        bigger[b].push_back(a);
+        smaller[a].push_back(b);
+    }
+
+
+    int res = 0;
+
+    for (int i = 1; i <= N; i++) {
+        countv = 0;
+        for (int t = 1; t <= N; t++) visit[t] = false;
+        dfs(i, true);
+        if (countv >= mid) {
+            res++;
+            continue;
+        }
+        countv = 0;
+        for (int t = 1; t <= N; t++) visit[t] = false;
+        dfs(i, false);
+        if (countv >= mid) res++;
+    }
+    cout << res;
+}
