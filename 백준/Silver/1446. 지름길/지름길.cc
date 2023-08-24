ce337e6d03b0d977732516656bad6a3494fda883
DongHyun <sunso514@naver.com>
2023-08-24 19:31:03 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#define test "test "
+#define endl "\n"
+#define GB " "
+
+#include <cstdlib>
+
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+struct Loc{
+    int start, end, cost;
+};
+
+int N, D;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N >> D;
+
+    vector<Loc> graph;
+    vector<int> dp(10001, 1000000007);
+
+
+    for (int i = 0; i < N; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        if (b > D) continue;
+        graph.push_back({ a, b, c });
+    }
+    
+    dp[0] = 0;
+
+    for (int i = 0; i < D; i++) {
+        for (Loc lc : graph) {
+            if (lc.start != i) continue;
+            dp[lc.end] = min(dp[lc.end], dp[i] + lc.cost);
+        }
+        dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+    }
+    cout << dp[D];
+}
