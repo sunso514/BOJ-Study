8a0cdb6b2ac9351d2e40cea4c71ab55f26ebdcbd
DongHyun <sunso514@naver.com>
2023-08-29 13:13:08 +0900

    [Silver III] Title: 달팽이, Time: 120 ms, Memory: 6000 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#define test "test "
+#define endl "\n"
+#define GB " "
+
+using namespace std;
+#define FOR(i, N) for(int (i) = 0; (i) < (N); (i)++)
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+vector<vector<int>> roll;
+int N;
+
+bool checkPass(int x, int y) {
+    if (x < 0 || y < 0 || x >= N || y >= N) return false;
+    if (roll[x][y] != 0) return false;
+    return true;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    
+    int K;
+    cin >> N >> K;
+
+    
+    roll.resize(N, vector<int>(N));
+
+    int dir = 0;
+    int nowx = 0;
+    int nowy = 0;
+    
+    pair<int, int> answer = { 0, 0 };
+
+    int count = N * N;
+    while (true)
+    {
+
+        roll[nowx][nowy] = count;
+        if (checkPass(nowx+dx[dir], nowy+dy[dir]) != true) dir = (dir + 1) % 4;
+
+
+        if (count == K) {
+            answer.first = nowx + 1;
+            answer.second = nowy + 1;
+        }
+        nowx += dx[dir];
+        nowy += dy[dir];
+
+
+        count--;
+        if (count == 0) break;
+    }
+
+    FOR(i, N) {
+        FOR(j, N) cout << roll[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << answer.first << " " << answer.second;
+
+
+}
