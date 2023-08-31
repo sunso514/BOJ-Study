0560f8238ca18444b95ba1bf40067aac75295264
DongHyun <sunso514@naver.com>
2023-08-31 20:37:21 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+cl = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+a = str(cl.index(input()))
+b = str(cl.index(input()))
+c = str(10**cl.index(input()))
+
+print(int(a+b+c[1:]))
