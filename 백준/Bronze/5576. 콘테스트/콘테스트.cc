ea084471d932b95052e8251b095de517980b6830
DongHyun <sunso514@naver.com>
2023-10-14 11:16:43 +0900

    [Bronze II] Title: 콘테스트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5576. 콘테스트/콘테스트.cc
+++ 백준/Bronze/5576. 콘테스트/콘테스트.cc
@@ -1,31 +1,15 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-int N, M;
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
 	vector<vector<int>> vec(2, vector<int>(10));
 	FOR(i, 2) FOR(j, 10) cin >> vec[i][j];
 	FOR(i, 2) sort(vec[i].begin(), vec[i].end());
-	int sum_A = vec[0][9] + vec[0][8] + vec[0][7];
-	int sum_B = vec[1][9] + vec[1][8] + vec[1][7];
-	cout << sum_A << " " << sum_B;
+	cout << vec[0][9] + vec[0][8] + vec[0][7] << " " << vec[1][9] + vec[1][8] + vec[1][7];
 }
