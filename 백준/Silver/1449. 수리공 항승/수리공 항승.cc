ea8d1189be750b32d6efeee67848a8376dfcb08c
DongHyun <sunso514@naver.com>
2023-10-17 07:22:41 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.cc
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -1,31 +1,12 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
-struct Location {
-	int x, y, time;
-};
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
 int N, M;
-
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -55,6 +36,5 @@ int main() {
 			tape = leak[point] - 0.5;
 		}
 	}
-
 	cout << ans;
 }
