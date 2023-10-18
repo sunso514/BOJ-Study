6e5c8b90914deb0a775efc2bd54c0988f88fa8d2
DongHyun <sunso514@naver.com>
2023-10-18 16:22:31 +0900

    [Silver IV] Title: 2+1 세일, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.cc
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.cc
@@ -1,31 +1,11 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
-struct Location {
-	int x, y, time;
-};
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
-int N, M;
-
-
+int N;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -39,10 +19,8 @@ int main() {
 	long long ans = 0;
 	int remove_ct = 0;
 
-	for (int i = 0; i < vec.size() % 3; i++) ans += vec[i];
-	//cout << vec.size() % 3 << endl;
+	FOR(i, vec.size()%3) ans += vec[i];
 	for (int i = N - 1; i > (vec.size() % 3); i--) {
-	
 		if ((i + 1 - vec.size() % 3) % 3 != 1) ans += vec[i];
 	}
 
