c87323d5f3d634b685a359d56b6cdf91d1093fe2
DongHyun <sunso514@naver.com>
2023-10-20 06:14:43 +0900

    [Silver III] Title: 화분 부수기, Time: 116 ms, Memory: 18668 KB -BaekjoonHub

--- 백준/Silver/3088. 화분 부수기/화분 부수기.cc
+++ 백준/Silver/3088. 화분 부수기/화분 부수기.cc
@@ -1,28 +1,11 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
-int N, M;
-
-
+int N;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
