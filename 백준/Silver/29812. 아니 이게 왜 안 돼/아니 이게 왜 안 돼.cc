9f0141707594b93346c760178bf8febc50fd0bab
DongHyun <sunso514@naver.com>
2023-10-22 12:41:14 +0900

    [Silver V] Title: 아니 이게 왜 안 돼, Time: 0 ms, Memory: 2652 KB -BaekjoonHub

--- 백준/Silver/29812. 아니 이게 왜 안 돼/아니 이게 왜 안 돼.cc
+++ 백준/Silver/29812. 아니 이게 왜 안 돼/아니 이게 왜 안 돼.cc
@@ -1,27 +1,12 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
 #include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
 #define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
 int N, D, M;
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
