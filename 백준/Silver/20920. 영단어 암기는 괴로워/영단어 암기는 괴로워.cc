b2b5f7bd6bb95834b1c7c59f4b76994329fc6517
DongHyun <sunso514@naver.com>
2023-10-30 14:18:36 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 112 ms, Memory: 13864 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -1,25 +1,10 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
 #include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
 #define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
 
 class Problem {
 public:
@@ -27,7 +12,6 @@ public:
 		for (int i = 0; i < N; i++) {
 			string get; 
 			cin >> get;
-			//if (mp.find(get) != mp.end()) 
 			if (get.length() >= M) mp[get]++;
 		}
 		vec = vector<pair<string, int>>(mp.begin(), mp.end());
