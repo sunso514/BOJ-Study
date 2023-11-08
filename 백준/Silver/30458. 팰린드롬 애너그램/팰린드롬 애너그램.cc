6f8f563d9041086c13bdcb6313df72adbdfe22f7
DongHyun <sunso514@naver.com>
2023-11-08 10:38:02 +0900

    [Silver IV] Title: 팰린드롬 애너그램, Time: 0 ms, Memory: 2648 KB -BaekjoonHub

--- 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.cc
+++ 백준/Silver/30458. 팰린드롬 애너그램/팰린드롬 애너그램.cc
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -33,9 +15,7 @@ int main() {
 
 	int nocheckidx = (N % 2 == 0) ? -1 : N / 2;
 	for (int i = 0; i < N; i++) {
-		if (nocheckidx == i) {
-			continue;
-		}
+		if (nocheckidx == i) continue;
 		vec[S[i] - 'a']++;
 	}
 
@@ -43,11 +23,8 @@ int main() {
 	bool chance = true;
 	bool pass = true;
 	for (int i = 0; i < 26; i++) {
-		//cout << vec[i] << " ";
 		if (vec[i] % 2 != 0) {
-			if (chance == true) {
-				chance = false;
-			}
+			if (chance == true) chance = false;
 			else {
 				pass = false;
 				break;
