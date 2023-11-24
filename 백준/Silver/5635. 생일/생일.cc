a0d8518b03bd0513915a9e03ae62b12fe56f6f80
DongHyun <sunso514@naver.com>
2023-11-24 17:52:12 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.cc
+++ 백준/Silver/5635. 생일/생일.cc
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include <algorithm>
@@ -19,18 +12,13 @@ struct JongGang{
 
 bool compare(JongGang haja, JongGang please){
     if (haja.year != please.year) return haja.year > please.year;
-    
     if (haja.month != please.month) return haja.month > please.month;
-    
     return haja.day > please.day;
-    
-    
 }
 
 
 int main()
 {
-    
     int N; cin >> N;
     vector<JongGang> please;
     for (int i = 0; i < N; i++){
@@ -40,8 +28,5 @@ int main()
         please.push_back({name, day, month, year});
     }
     sort(please.begin(), please.end(), compare);
-    cout << please.front().name << "\n";
-    cout << please.back().name;
-    
-
+    cout << please.front().name << "\n" << please.back().name;
 }
