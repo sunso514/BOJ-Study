39b7c11720949d90944c4895e88daee7f2f89eb8
DongHyun <sunso514@naver.com>
2023-11-26 16:05:46 +0900

    [Silver V] Title: 카약, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2890. 카약/카약.cc
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct KY {
+    int distance;
+    int team;
+    int rank;
+};
+
+bool compare(KY a, KY b) {
+    return a.distance > b.distance;
+}
+
+bool compare_by_index(KY a, KY b) {
+    return a.team < b.team;
+}
+
+
+int main()
+{
+    int R, C;
+    cin >> R >> C;
+
+    vector<KY> res;
+
+    for (int i = 0; i < R; i++) {
+        string track; cin >> track;
+
+        int count = 0;
+        int team = -1;
+        for (int j = 1; j < C - 1; j++) {
+            if (track[j] == '.') count++;
+            else {
+                team = track[j] - '0';
+                break;
+            }
+        }
+
+        if (team != -1) {
+            res.push_back({ count, team, 0 });
+        }
+    }
+    sort(res.begin(), res.end(), compare);
+    int rank = 0;
+    int last = -1;
+    for (int i = 0; i < 9; i++) {
+        if (last != res[i].distance) {
+            rank++;
+        }
+        last = res[i].distance;
+        res[i].rank = rank;
+    }
+
+    sort(res.begin(), res.end(), compare_by_index);
+    for (int i = 0; i < 9; i++) {
+        cout << res[i].rank << "\n";
+    }
+
+}
