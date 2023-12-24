7037e69f5d674372445ece006da59c5fe96c57c1
DongHyun <sunso514@naver.com>
2023-12-24 09:00:20 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 16 ms, Memory: 5996 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
@@ -1,23 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#define test "test "
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -33,7 +18,6 @@ int main() {
     vec[0][0] = 1;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-
             for (int k = 0; k < 3; k++) {
                 int tmp_x = i + dx[k];
                 int tmp_y = j + dy[k];
@@ -41,11 +25,8 @@ int main() {
                 if (tmp_x < 0 || tmp_y < 0 || tmp_x >= N || tmp_y >= M) continue;
 
                 vec[i][j] = (vec[tmp_x][tmp_y] + vec[i][j]) % 1000000007;
-
             }
-
         }
     }
-
     cout << vec[N - 1][M - 1];
 }
