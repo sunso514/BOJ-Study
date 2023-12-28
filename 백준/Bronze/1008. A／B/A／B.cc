cbefc9a80affda24b18112feb2f1889e6b849fc5
DongHyun <sunso514@naver.com>
2023-12-29 02:37:41 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+
+#define test "test "
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double a, b;
+    cin >> a >> b;
+
+    cout.precision(10);
+    cout << a / b;
+}
