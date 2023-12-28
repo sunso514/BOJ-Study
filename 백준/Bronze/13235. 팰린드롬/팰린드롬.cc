aa0152e29f8c09c0b599099f66915e9054205fc2
DongHyun <sunso514@naver.com>
2023-12-29 00:53:41 +0900

    [Bronze II] Title: 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.cc
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.cc
@@ -1,22 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#define test "test "
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
 
 int main() {
     ios_base::sync_with_stdio(false);
