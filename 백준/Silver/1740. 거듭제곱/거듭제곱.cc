92db8d06f2d4408df2df1e317dc5a14ddcb8007f
DongHyun <sunso514@naver.com>
2024-01-05 14:45:04 +0900

    [Silver IV] Title: 거듭제곱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1740. 거듭제곱/거듭제곱.cc
+++ 백준/Silver/1740. 거듭제곱/거듭제곱.cc
@@ -1,30 +1,14 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
 #include <bitset>
 
-#define test "test "
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
 long long powV(long long a, long long b) {
     long long res = 1;
-    
-    for (int i = 0; i < b; i++) {
-        res *= a;
-    }
+    for (int i = 0; i < b; i++) res *= a;
     return res;
 }
 
@@ -35,18 +19,14 @@ int main() {
     long long N;
     cin >> N;
     string iToBit = bitset<42>(N).to_string();
-    //cout << iToBit;
     long long res = 0;
 
     int now;
     for (int i = 0; i < 42; i++) {
         now = iToBit.length()-i-1;
-        //cout << iToBit[now];
         if (iToBit[now] == '1') {
-
             res += powV(3, i);
         }
     }
-    //cout << endl;
     cout << res;
 }
