4ddcc0c5d7eff4454c5bee49792ca9be7f61fa4d
DongHyun <sunso514@naver.com>
2024-01-18 15:41:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
+    int answer = health;
+    
+    int streak = 0;
+    int point = 0;
+    for (int i = 0; i < attacks[attacks.size()-1][0]+1; i++){
+        if (i == attacks[point][0]){
+            answer -= attacks[point][1];
+            if (answer <= 0) return -1;
+            streak = 0;
+            point++;
+        }
+        else{
+            streak++;
+            answer = min(health, answer + bandage[1]);
+            if (streak == bandage[0]){
+                streak = 0;
+                answer = min(health, answer + bandage[2]);
+            }
+        }
+    }
+        
+    return answer;
+}
