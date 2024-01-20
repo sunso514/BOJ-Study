0042077471fa971dddf7f8034c6ac2846ad7c287
DongHyun <sunso514@naver.com>
2024-01-20 15:24:38 +0900

    [Silver IV] Title: Byte Coin, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17521. Byte Coin/Byte Coin.cc
+++ 백준/Silver/17521. Byte Coin/Byte Coin.cc
@@ -19,37 +19,37 @@ int main() {
     for (int i = 0; i < N; i++) cin >> price[i];
 
 
-    vector<long long> inflection_point;
+    vector<long long> vertex;
     // -1 : 하락, 0 : X , 1: 상승
     int info = 0;
-    inflection_point.push_back(price[0]);
+    vertex.push_back(price[0]);
     for (int i = 1; i < N; i++) {
         //상승
         if (price[i] > price[i - 1]) {
-            if (info == -1) inflection_point.push_back(price[i - 1]);
+            if (info == -1) vertex.push_back(price[i - 1]);
             info = 1;
         }
         //하락
         else if (price[i] < price[i - 1]) {
-            if (info == 1) inflection_point.push_back(price[i - 1]);
+            if (info == 1) vertex.push_back(price[i - 1]);
             info = -1;
         }
 
-        if (i == N - 1 && info == 1) inflection_point.push_back(price[i]);
+        if (i == N - 1 && info == 1) vertex.push_back(price[i]);
     }
 
     
     long long amount = 0;
-    for (int i = 0; i < inflection_point.size()-1; i++) {
-        if (inflection_point[i] < inflection_point[i + 1] && W >= inflection_point[i]) {
-            amount += W / inflection_point[i];
-            W -= inflection_point[i] * amount;
+    for (int i = 0; i < vertex.size()-1; i++) {
+        if (vertex[i] < vertex[i + 1] && W >= vertex[i]) {
+            amount += W / vertex[i];
+            W -= vertex[i] * amount;
         }
         else {
-            W += inflection_point[i] * amount;
+            W += vertex[i] * amount;
             amount = 0;
         }
     }
-    W += inflection_point[inflection_point.size() - 1] * amount;
+    W += vertex[vertex.size() - 1] * amount;
     cout << W;
 }
