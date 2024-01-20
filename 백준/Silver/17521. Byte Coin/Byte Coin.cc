4ccc0aff3a2f738bd1f2357a29671627c3b52000
DongHyun <sunso514@naver.com>
2024-01-20 15:21:20 +0900

    [Silver IV] Title: Byte Coin, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17521. Byte Coin/Byte Coin.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+
+#include <bitset>
+
+#define test "test "
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
+
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long N, W;
+    cin >> N >> W;
+
+    if (N == 1) {
+        cout << W;
+        return 0;
+    }
+
+    vector<long long> price(N);
+    for (int i = 0; i < N; i++) {
+        cin >> price[i];
+    }
+
+
+    vector<long long> inflection_point;
+    // -1 : 하락, 0 : X , 1: 상승
+    int info = 0;
+    inflection_point.push_back(price[0]);
+    for (int i = 1; i < N; i++) {
+        //상승
+        if (price[i] > price[i - 1]) {
+            if (info == -1) inflection_point.push_back(price[i - 1]);
+            info = 1;
+        }
+        //하락
+        else if (price[i] < price[i - 1]) {
+            if (info == 1) inflection_point.push_back(price[i - 1]);
+            info = -1;
+        }
+
+        if (i == N - 1 && info == 1) inflection_point.push_back(price[i]);
+    }
+
+    
+    long long amount = 0;
+    for (int i = 0; i < inflection_point.size()-1; i++) {
+        if (inflection_point[i] < inflection_point[i + 1] && W >= inflection_point[i]) {
+            amount += W / inflection_point[i];
+            W -= inflection_point[i] * amount;
+        }
+        else {
+            W += inflection_point[i] * amount;
+            amount = 0;
+        }
+    }
+    W += inflection_point[inflection_point.size() - 1] * amount;
+    cout << W;
+}
