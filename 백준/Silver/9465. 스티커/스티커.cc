19bd24a84c72d77a1d3034a2e865eac876747302
DongHyun <sunso514@naver.com>
2024-02-07 15:23:35 +0900

    [Silver I] Title: 스티커, Time: 96 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,21 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#include <bitset>
-
-#define test "test "
 #define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
@@ -52,7 +39,4 @@ int main() {
         }
         cout << maxv << endl;
     }
-
 }
-
-
