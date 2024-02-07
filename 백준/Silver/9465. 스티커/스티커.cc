258610c0e3ddb67ef37bdd7c3114b2a8e8e0dbb4
DongHyun <sunso514@naver.com>
2024-02-07 15:21:14 +0900

    [Silver I] Title: 스티커, Time: 96 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+
+#include <bitset>
+
+#define test "test "
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T;
+    cin >> T;
+
+    for (int tc = 0; tc < T; tc++) {
+        int a; cin >> a;
+        vector<vector<int>> stk(2, vector<int>(a));
+        
+        for (int i = 0; i < a; i++) cin >> stk[0][i];
+        for (int i = 0; i < a; i++) cin >> stk[1][i];
+        if (a == 1) {
+            cout << max(stk[0][0], stk[1][0]) << endl;
+            continue;
+        }
+        vector<vector<int>> dp = stk;
+
+        int maxv = 0;
+        for (int i = 0; i < a - 1; i++) {
+            dp[0][i + 1] = max(dp[1][i] + stk[0][i + 1], dp[0][i + 1]);
+            dp[1][i + 1] = max(dp[0][i] + stk[1][i + 1], dp[1][i + 1]);
+            maxv = max(max(dp[0][i + 1], dp[1][i + 1]), maxv);
+            if (i < a - 2) {
+                for (int j = 0; j < 4; j++) {
+                    dp[j / 2][i + 2] = max(dp[j % 2][i] + stk[j / 2][i + 2], dp[j / 2][i + 2]);
+                    maxv = max(dp[j / 2][i + 2], maxv);
+                }
+            }
+        }
+        cout << maxv << endl;
+    }
+
+}
+
+
