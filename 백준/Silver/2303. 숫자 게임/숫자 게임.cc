7a5993576b2183abdedaa963863f43498bd3b982
DongHyun <sunso514@naver.com>
2024-03-07 11:16:22 +0900

    [Silver V] Title: 숫자 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2303. 숫자 게임/숫자 게임.cc
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.cc
@@ -2,56 +2,11 @@
 #include <algorithm>
 #include <cstring>
 #include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#include <bitset>
-
-#define test "test "
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-
-
-vector<vector<int>> graph;
-vector<int> visit(10001);
-vector<pair<int, int>> ans;
-queue<int> que;
-
-// 풀이 로직
-/**
- dynamic programming 처럼 해보기..?
- **/
-int maxscr = 0;
-
-int bfs(int n) {
-
-    visit.assign(10001, false);
-    que.push(n);
-    visit[n] = true;
-    int hax = 0;
-    while (!que.empty()) {
-        int loc = que.front();
-        que.pop();
-        for (auto i : graph[loc]) {
-            if (visit[i] == false) {
-                visit[i] = true;
-                que.push(i);
-                hax++;
-            }
-        }
-    }
-    maxscr = max(hax, maxscr);
-    return hax;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
