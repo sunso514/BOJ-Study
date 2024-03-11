03bdb01f040add2fc9da760857cbd66e2e4e5004
DongHyun <sunso514@naver.com>
2024-03-11 09:51:00 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 12 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.cc
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.cc
@@ -1,20 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
-#include <bitset>
-
-#define test "test "
-#define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
@@ -33,11 +20,8 @@ int main() {
     FOR(i, N) {
         cin >> pay[i+1];
     }
-    nujuk[0] = 0;
-    nujuk[1] = pay[1];
     for (int i = 1; i <= N; i++) {
         nujuk[i] = pay[i] + nujuk[i - 1];
-
     }
     long long ans = 0;
     for (int i = M; i <= N; i++) {
