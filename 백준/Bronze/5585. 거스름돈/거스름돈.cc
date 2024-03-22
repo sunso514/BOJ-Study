a71c1408e6e9bfd7375e96c0485db77265f61d71
DongHyun <sunso514@naver.com>
2024-03-23 08:46:10 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -1,30 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
-
 using namespace std;
-
-#define TEST cout << "test\n";
-#define X2 (fb.x + (dx[fb.direction] == -1 ? N - 1 : dx[fb.direction]) * fb.speed) % N
-#define Y2 (fb.y + (dy[fb.direction] == -1 ? N - 1 : dy[fb.direction]) * fb.speed) % N
-
-
-int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-int dy[8] = { 0, 1, 1, 1, 0, -1 , -1, -1 };
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -32,12 +8,9 @@ int main() {
 	
 	int N = 0;
 	cin >> N;
-
 	N = 1000 - N;
-
-
+    
 	int count = 0;
-	
 	int a[6] = { 500, 100, 50, 10, 5, 1 };
 	
 	for (int i = 0; i < 6; i++) {
