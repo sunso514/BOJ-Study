4b491fbcb6b9c0c29f78c4b5e17379adce232f31
DongHyun <sunso514@naver.com>
2024-07-09 17:57:15 +0900

    [Bronze II] Title: 24, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.cc
+++ 백준/Bronze/1408. 24/24.cc
@@ -2,30 +2,8 @@
 #include <algorithm>
 #include <cstring>
 #include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-int N, M;
-
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
 
 string getZero(int N) {
 	if (N < 10) {
@@ -49,8 +27,5 @@ int main() {
 	if (atime > btime) btime += 86400;
 	tottime = btime - atime;
 
-	//cout << tottime / 3600 << " " << tottime / 60 % 60 << " " << tottime % 60 << endl;
-	cout << getZero(tottime / 3600) << ":" << getZero(tottime / 60 % 60) << ":" << getZero(tottime % 60);
-
-	
+	cout << getZero(tottime / 3600) << ":" << getZero(tottime / 60 % 60) << ":" << getZero(tottime % 60);	
 }
