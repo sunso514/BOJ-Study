af47cbad7f6c2c11d9af2b74d4898a283976e5b3
DongHyun <sunso514@naver.com>
2024-07-10 13:53:20 +0900

    [Silver V] Title: 인기 투표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11637. 인기 투표/인기 투표.cc
+++ 백준/Silver/11637. 인기 투표/인기 투표.cc
@@ -1,40 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
-
 using namespace std;
-
-int N, M;
-
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
-string getZero(int N) {
-	if (N < 10) {
-		string tmp = "0";
-		tmp.push_back(N + '0');
-		return tmp;
-	}
-	return to_string(N);
-}
 
 int main() {
 	ios::sync_with_stdio(false);
