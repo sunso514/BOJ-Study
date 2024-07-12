cafdc8ee3e6f8b980fd185acaecb8e481041385f
DongHyun <sunso514@naver.com>
2024-07-12 12:44:35 +0900

    [Silver III] Title: 줄줄이 박수, Time: 268 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.cc
+++ 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.cc
@@ -1,32 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-int N, M;
-
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-#define X dx[i] + loc.x
-#define Y dy[i] + loc.y
-
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -48,12 +24,6 @@ int main() {
 
 	int range;
 	cin >> range;
-	  
-
-	//for (int i = 0; i < M + 1; i++) {
-	//	cout << sum[i] << " ";
-	//}
-	//cout << endl;
 
 	long long maxV = -1;
 	for (int i = 0; i + range <= M; i++) {
