eb6356998de2efb33ddef273bd01067c2dfbe385
DongHyun <sunso514@naver.com>
2024-07-23 14:33:59 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,21 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
@@ -38,12 +23,7 @@ int main() {
 		cin >> time_table[i].start >> time_table[i].end;
 	}
 	sort(time_table.begin(), time_table.end(), compare);
-	/*
-	for (auto i : time_table) {
-		cout << i.start << " " << i.end << endl;
-	}
-	cout << "\nTEST\n";
-	*/
+    
 	int ans = 0;
 	int now_time = -1;
 	for (auto lec : time_table) {
