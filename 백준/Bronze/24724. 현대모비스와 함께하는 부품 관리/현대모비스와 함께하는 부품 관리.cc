a08fff583f49bb33efeff9115168704c9cbc1d7b
DongHyun <sunso514@naver.com>
2024-07-28 21:34:41 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int T, N, la, lb, A, B;
+    cin >> T;
+    for(int i = 1; i <= T; i++){
+        cin >> N >> la >> lb;
+        for(int j = 0; j < N; j++){
+            cin >> A >> B;
+        }
+        cout << "Material Management " << i << '\n';
+        cout << "Classification ---- End!\n";
+    }
+    
+}
