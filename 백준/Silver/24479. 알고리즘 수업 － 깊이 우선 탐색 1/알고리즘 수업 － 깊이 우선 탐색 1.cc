e38ad152d747f7f1a1890573ddc8f8e1d5271606
DongHyun <sunso514@naver.com>
2024-08-05 14:21:18 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 80 ms, Memory: 10932 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -1,21 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
 #define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
 
@@ -30,9 +16,7 @@ void dfs(int st) {
 
 	for (auto n : graph[st]) {
 		if (visit[n] != 0) continue;
-		else {
-			dfs(n);
-		}
+		dfs(n);
 	}
 }
 
@@ -49,14 +33,16 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		int u, v;
 		cin >> u >> v;
-		
 		graph[u].push_back(v);
 		graph[v].push_back(u);
 	}
+    
 	for (int i = 1; i <= N; i++) {
 		sort(graph[i].begin(), graph[i].end());
 	}
+    
 	dfs(R);
+    
 	for (int i = 1; i <= N; i++) {
 		cout << visit[i] << endl;
 	}
