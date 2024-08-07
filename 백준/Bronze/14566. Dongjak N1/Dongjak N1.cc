6a145dd768a4988734419bd2db563b87ffcae5f6
DongHyun <sunso514@naver.com>
2024-08-07 18:36:34 +0900

    [Bronze I] Title: Dongjak N1, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Bronze/14566. Dongjak N1/Dongjak N1.cc
+++ 백준/Bronze/14566. Dongjak N1/Dongjak N1.cc
@@ -1,41 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
 using namespace std;
-
-vector<vector<int>> graph;
-vector<int> visit;
-int cnt = 1;
-
-void dfs(int st) {
-	if (visit[st] != 0) return;
-	visit[st] = cnt;
-	cnt++;
-
-	for (auto n : graph[st]) {
-		if (visit[n] != 0) continue;
-		else {
-			dfs(n);
-		}
-	}
-}
-
 
 int main() {
 	ios::sync_with_stdio(false);
