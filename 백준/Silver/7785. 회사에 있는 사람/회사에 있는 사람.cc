a8feda77857a23542ad0931c4f16fb14d22e8465
DongHyun <sunso514@naver.com>
2024-08-27 14:42:14 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 76 ms, Memory: 13168 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
 #include <stack>
-#include <cmath>
 #include <map>
-#include <deque>
-#include <set>
-#include <tuple>
 
 #include <bitset>
 
@@ -20,34 +12,18 @@
 using namespace std;
 
 
-pair<int, int> get_letters(string N) {
-	int small = 0;
-	int big = 0;
-	for (auto i : N) {
-		if (i >= 'a' && i <= 'z') small++;
-		if (i >= 'A' && i <= 'Z') big++;
-	}
-	return { small, big };
-}
-
-
-
-
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	int N;
 	cin >> N;
 
-	vector<int> names;
 	map<string, bool> m;
 
 	for (int i = 0; i < N; i++) {
 		string a, b; cin >> a >> b;
 		m[a] = (b == "enter");
 	}
-
 
 	stack<string> ans;
 	for (auto i : m) {
@@ -57,6 +33,4 @@ int main() {
 		cout << ans.top() << endl;
 		ans.pop();
 	}
-
-
 }
