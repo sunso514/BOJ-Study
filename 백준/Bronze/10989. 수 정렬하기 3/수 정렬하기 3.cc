2eeb1a986d753721a55bfd2024fd3780cced6879
DongHyun <sunso514@naver.com>
2024-09-03 13:36:08 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1476 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,20 +8,20 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n;
-    cin >> n;
-    int list[10001]={0};
-
-    for(int i=0;i<n;i++) {
-        int x;
-        cin >> x;
-        list[x]= list[x]+1;
+    int N;
+    cin >> N;
+    
+    vector<long long> vec(10001, 0);
+    
+    for(int i = 0; i <N; i++){
+        int a; cin >> a;
+        vec[a]++;
     }
-
-    for (int i=1; i<10001;i++) {
-        for (int j=0; j<list[i];j++) {
-            cout << i << '\n';
+    for(int i = 1; i < 10001; i++){
+        for(int j = 0; j < vec[i]; j++){
+            cout << i << "\n";
         }
     }
+    
 
 }
