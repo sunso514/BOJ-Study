074d30ce26c8bb9e3366489be3ede2ae286ed970
DongHyun <sunso514@naver.com>
2025-01-11 11:17:17 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,34 +1,23 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <vector>
 #include <stack>
 using namespace std;
-//	vector<int> list(time);
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
     
-    stack<int> stack;
-
-    int time;
-    cin >> time;
-    int a;
-    for (int i = 0; i < time; i++) {
+    stack<int> stk;
+    int a, N; cin >> N;
+    for (int i = 0; i < N; i++) {
         cin >> a;
-        if (a == 0) stack.pop();
-        else stack.push(a);
+        if (a == 0) stk.pop();
+        else stk.push(a);
     }
     int res = 0;
-    int size = stack.size();
-//while (!stack.empty()) 
+    int size = stk.size();
     for (int i = 0; i < size; i++) {
-        res += stack.top();
-        stack.pop();
+        res += stk.top();
+        stk.pop();
     }
     cout << res;
 }
