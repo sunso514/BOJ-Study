16c60d35b4dc3f6172bea5df35c83900770adce6
DongHyun <sunso514@naver.com>
2025-01-14 18:00:24 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.cc
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+
+#include <bitset>
+
+#define debug_msg cout << "Test";
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
+
+using namespace std;
+
+int N; 
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string N; cin >> N;
+
+    stack<int> stk;
+
+    int fail = 0;
+    for (auto c : N) {
+        int code = c - '(';
+        if (stk.empty()) {
+            if (code == 1) fail++;
+            else stk.push(code);
+            continue;
+        }
+
+        if (stk.top() == (code + 1) % 2) {
+            stk.pop();
+        }
+        else {
+            stk.push(code);
+        }
+    }
+    cout << fail + stk.size();
+
+}
+    
