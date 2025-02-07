8ba6b95e0444c717aa805a092ffddab241db8093
DongHyun <sunso514@naver.com>
2025-02-07 10:57:47 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
@@ -1,33 +1,7 @@
-
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
-#define endl "\n"
-#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
-
 using namespace std;
-
-
-bool checkS(int point, string N, string cmp) {
-    for (int i = 0; i < cmp.length(); i++) {
-        if (N[i + point] != cmp[i]) return false;
-    }
-    return true;
-}
-
 
 int main(void) {
     cin.tie(NULL);
