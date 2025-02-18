9bdd7a39483a57de25b675f460bec988a4cbf78e
DongHyun <sunso514@naver.com>
2025-02-18 17:44:26 +0900

    [Silver III] Title: Closing the Loop (Large), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/12538. Closing the Loop （Large）/Closing the Loop （Large）.cc
+++ 백준/Silver/12538. Closing the Loop （Large）/Closing the Loop （Large）.cc
@@ -1,20 +1,8 @@
-
 #include <iostream>
 #include <algorithm>
 #include <cstring>
 #include <string>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <cmath>
-#include <map>
-#include <deque>
-#include <set>
-#include <tuple>
-
-#include <bitset>
-
-#define debug_msg cout << "Test";
 #define endl "\n"
 #define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 
