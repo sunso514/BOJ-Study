6047df6200ee25a7b3adf4ee297e9f2b9d2c0e66
DongHyun <sunso514@naver.com>
2025-02-19 12:16:41 +0900

    [Silver V] Title: Closing the Loop (Small), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/12537. Closing the Loop （Small）/Closing the Loop （Small）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
+
+using namespace std;
+
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N; cin >> N;
+
+    FOR (tc, N) {
+        int S; cin >> S;
+        vector<int> red;
+        vector<int> blue;
+
+        FOR(i, S) {
+            string get; cin >> get;
+            if (get[get.size() - 1] == 'R') {
+                get.pop_back();
+                red.push_back(stoi(get));
+            }
+            else {
+                get.pop_back();
+                blue.push_back(stoi(get));
+            }
+        }
+
+        sort(red.rbegin(), red.rend());
+        sort(blue.rbegin(), blue.rend());
+        int ans = 0;
+        FOR(i, min(red.size(), blue.size())) {
+            ans += red[i] + blue[i] - 2;
+        }
+        cout << "Case #" << tc + 1 << ": " << ans << "\n";
+    }
+
+}
+    
