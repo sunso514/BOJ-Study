b8ddec1b5f0af8f247660f66d1f6a557d8df71f3
DongHyun <sunso514@naver.com>
2025-02-22 15:33:48 +0900

    [Silver I] Title: 오목, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.cc
+++ 백준/Silver/2615. 오목/오목.cc
@@ -24,43 +24,6 @@ using namespace std;
 int dx[4] = { -1, 0, 1, 1 };
 int dy[4] = { 1, 1, 1, 0 };
 
-
-void doBan(int dol, int i, int j, vector<vector<int>> &ch, vector<vector<vector<int>>>&bn, int type) {
-
-    int k = 0;
-
-    while (true) {
-        k++;
-        // 오른쪽 5개인 경우
-        if (type == 0) {
-            if (j + k > 18) break;
-            if (dol != ch[i][j + k]) break;
-            bn[i][j + k][type] = true;
-        }
-
-        // 대각선 오른쪽 5개인 경우
-        if (type == 1) {
-            if (j + k > 18 || i + k > 18) break;
-            if (dol != ch[i + k][j + k]) break;
-            bn[i + k][j + k][type] = true;
-        }
-
-        // 아래 5개인 경우
-        if (type == 2) {
-            if (i + k > 18) break;
-            if (dol != ch[i + k][j]) break;
-            bn[i + k][j][type] = true;
-        }
-
-        // 대각선 오른쪽 위 5개인 경우
-        if (type == 3) {
-            if (j + k > 18 || i + k > 18) break;
-            if (dol != ch[i + k][j + k]) break;
-            bn[i + k][j + k][type] = true;
-        }
-    }
-}
-
 int main(void) {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
@@ -87,10 +50,13 @@ int main(void) {
                 streak++;
             }
 
+            // 5목일 경우
             if (streak == 5) {
                 cout << dol << endl << i + 1 << " " << j + 1;
                 return 0;
             }
+
+            // 6목 이상일 경우
             if (streak == 6) {
                 for (int p = 0; p <= 18; p++) {
                     int tmp_x = i + p * dx[k];
@@ -103,7 +69,6 @@ int main(void) {
             
         }
     }
-
     cout << 0;
 }
     
