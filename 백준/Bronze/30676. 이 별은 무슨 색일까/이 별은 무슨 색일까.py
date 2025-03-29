2f34ac8857c140ff7c5f83bdbb7204872dd6e1a6
DongHyun <sunso514@naver.com>
2025-03-29 22:32:29 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+w = int(input())
+if 620 <= w <= 780:
+    print("Red")
+elif 590 <= w< 620:
+    print("Orange")
+elif 570 <= w < 590:
+    print("Yellow")
+elif 495 <= w < 570:
+    print("Green")
+elif 450 <= w < 495:
+    print("Blue")
+elif 425 <= w < 450:
+    print("Indigo")
+elif 380 <= w< 425:
+    print("Violet")
