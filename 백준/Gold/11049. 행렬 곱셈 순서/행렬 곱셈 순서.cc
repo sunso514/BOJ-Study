0866da51648f570434315ce8f0947e8da2af83f6
DongHyun <sunso514@naver.com>
2025-05-12 21:51:43 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 40 ms, Memory: 3080 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -1,21 +1,39 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
 
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <set>
+#include <tuple>
+#include <iomanip>
+
+#include <bitset>
+
+
+#define debug_msg cout << "Test";
+#define endl "\n"
+#define FOR(a, b) for(int (a) = 0; (a) < (b); (a)++)
 #define INF 1000000009
 
-int N, r, c;
+using namespace std;
 
-int main()
-{
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	int N, r, c;
+	
 	cin >> N;
-    vector<vector<int>> dp(N+2, vector<int>(N+2));
-    vector<vector<int>> mat(N+2, vector<int>(2));
-    vector<int> sum(N+2);
-    
-	for (int i = 1; i <= N; i++)
-	{
+	vector<vector<int>> dp(N + 1, vector<int>(N + 1));
+	vector<vector<int>> mat(N + 1, vector<int>(2));
+
+	for (int i = 1; i <= N; i++) {
 		cin >> r >> c;
 		mat[i][0] = r;
 		mat[i][1] = c;
@@ -26,12 +44,13 @@ int main()
 		for (int j = 1; i + j <= N; j++)
 		{
 			dp[j][i + j] = INF;
-			for (int k = j; k <= i + j; k++)
+			for (int k = j; k < i + j; k++)
 			{
-				dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + mat[j][0] * mat[k][1] * mat[i+j][1]);
+				dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + mat[j][0] * mat[k][1] * mat[i + j][1]);
 			}
 		}
 
 	}
+
 	cout << dp[1][N];
 }
