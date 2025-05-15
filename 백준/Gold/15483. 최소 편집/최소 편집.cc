5f29092b15feace4762211b4a92fe28f5e41b59d
DongHyun <sunso514@naver.com>
2025-05-15 15:13:53 +0900

    [Gold III] Title: 최소 편집, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/15483. 최소 편집/최소 편집.cc
+++ 백준/Gold/15483. 최소 편집/최소 편집.cc
@@ -39,8 +39,7 @@ int main(void) {
 
     for (int i = 1; i <= m; i++) {
         for (int j = 1; j <= n; j++) {
-            if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-            else dp[i][j] = min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+            dp[i][j] = min(min(dp[i][j - 1] + 1, dp[i - 1][j] + 1), dp[i - 1][j - 1] + (a[i-1] != b[j-1]));
         }
     }
  
