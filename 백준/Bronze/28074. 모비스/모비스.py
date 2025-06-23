22c3396eb4547806857d27b1e0fa2ff70370220e
DongHyun <sunso514@naver.com>
2025-06-23 23:40:36 +0900

    [Bronze IV] Title: 모비스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.py
@@ -0,0 +1,16 @@
+N = input()
+can = True
+if 'M'  not in N:
+	can = False	
+if 'O'  not in N:
+	can = False
+if 'B'  not in N:
+	can = False
+if 'I'  not in N:
+	can = False
+if 'S'  not in N:
+	can = False
+if can == True:
+	print("YES")
+else:
+	print("NO")
